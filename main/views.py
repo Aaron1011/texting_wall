4596c3fa9d4f48e771c4f5c0d3fd74d4f08af796
Aaron Hill <aa1ronham@aol.com>
2012-10-01 18:07:31 -0400

    CSRF Error

--- main/views.py
+++ main/views.py
@@ -1,10 +1,33 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from django.core.context_processors import csrf
+from django.http import HttpResponse
+from django.core.context_processors import csrf
+my_choice = (
+    ('BG', 'Blog'),
+    ('FR', 'Friend'),
+    ('OT', 'Other',)
+)
+
+class UserCreation(UserCreationForm):
+    about_textwall = forms.ChoiceField(my_choice)
 
 def create_account(request):
-    form = UserCreationForm()
-    return render_to_response(
-        "create_account.html",
+    c = {}
+    c.update(csrf(request))
+    if request.POST:
+        return render_to_response("finish.html", c) 
+    else:
+        choices = (
+        ('BG', 'Blog'),
+        ('FR', 'Friend'),
+        ('OT', 'Other',
+))
+        form = UserCreation()
+        return render_to_response(
+    "create_account.html",
         { "form": form })
-
+ 
--- texting_wall/urls.py
+++ texting_wall/urls.py
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^create_account/', 'main.views.create_account'))
+    url(r'^create_account/', 'main.views.create_account'),
+    url(r'^finish/', 'main.views.process'))
