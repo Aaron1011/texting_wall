4f1dba7344ed334c69afd44e361734d110b16175
Aaron Hill <aa1ronham@aol.com>
2012-10-09 19:21:15 -0400

    Added ForeignKey to Wall model

--- main/models.py
+++ main/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import string, random
 # Create your models here.
 
 class Wall(models.Model):
-    name = models.CharField(max_length=20)
-    hashtag = models.CharField(max_length=20)
+    def get_hashtag():
+        return "".join([random.choice(string.lowercase) for i in range(3)])
+    sms_keyword = models.CharField(max_length=20, help_text=('Keyword to start messages with'))
+    hashtag = models.CharField(max_length=20, help_text='Twitter hashtag to tweet to', default=get_hashtag())
+    user =  models.ForeignKey(User, editable=True)
 
     def __unicode__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,16 +1,17 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from django.contrib.auth import forms as auth_forms
-
 from django.core.context_processors import csrf
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.http import HttpResponse
+from forms import WallForm
 import forms as local_forms
+from django.contrib.auth.decorators import login_required
+
 
 def create_account(request):
     form = local_forms.UserCreation()
@@ -28,10 +29,14 @@ def create_account(request):
         { "form": form }, RequestContext(request))
 def finish(request):
     return HttpResponse("Login Successfull!")
-
+@login_required(login_url="/login", redirect_field_name='/create_wall' )
 def new_wall(request):
     if request.POST:
-        pass
+        f = WallForm(data=request.POST)
+        wallform = f.save(commit=False)
+        wallform.user.username = request.user
+        wallform.save()
+        return HttpResponse("Hello, World!")
     else:
         form = local_forms.WallForm()
         return render_to_response(
