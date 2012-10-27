a2524cfe96b1924665897f8953bfc7ff17608e53
Aaron Hill <aa1ronham@aol.com>
2012-10-27 10:27:45 -0400

    Protect against Twitter returning unexpected data

--- listen_for_tweets.py
+++ listen_for_tweets.py
@@ -8,23 +8,10 @@ setup_environ(settings)
 from main.models import Wall
 from main import Pubnub
 import json, re
-
-PUBLISH_KEY = "pub-8a8223f4-631c-4484-a118-2b01232307cc"
-SUBSCRIBE_KEY = "sub-e754ed6b-133d-11e2-91f2-b58e6c804094"
-SECRET = "sec-ZjcxZGVjNDAtZWQyMC00MGZmLTg1Y2MtNmJkNGE3YTJiYjlj"
-pubnub = Pubnub.Pubnub(PUBLISH_KEY, SUBSCRIBE_KEY, SECRET, False)
+import time
+from django.conf import settings as app_settings
+pubnub = Pubnub.Pubnub(app_settings.PUBLISH_KEY, app_settings.SUBSCRIBE_KEY, app_settings.SECRET, False)
 hashtags = []
-monitored = []
-
-# Go to http://dev.twitter.com and create an app. 
-# The consumer key and secret will be generated for you after
-consumer_key="3yyqRg7yqmyIZvzOzJI20w"
-consumer_secret="lPGDVv8dSctExxkXOAqXUnQKEJPBiUJq791XC8PGeI"
-
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
-access_token="879862038-DAGmivCACAap96mZxPi4YfIRwM3Zv3TnG4SGyQYN"
-access_token_secret="Olwot3CAYkPUfIVOf2pdAk2LDmN9gsd255B0Xagtjo"
 
 
 
@@ -36,22 +23,25 @@ class PubnubListener(StreamListener):
     """
     def on_data(self, data):
         message = json.loads(data)
-        tweet = re.match("(\S*)\s(.*)", str(message['text']))
-        wall = Wall.objects.filter(hashtag__exact=tweet.group(1))
-        pubnub.publish({
+        if message.has_key('text'):
+            tweet = re.match("(\S*)\s(.*)", str(message['text']))
+            wall = Wall.objects.filter(hashtag__exact=tweet.group(1))
+            pubnub.publish({
             'channel' : wall[0].sms_keyword,
             'message' : {
                 'message' : tweet.group(2)
-            }
-        })
+                }
+            })
+        else:
+            print message
         return True
 
     def on_error(self, status):
         print status
 
 l = PubnubListener()
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
 
 def main():
     while True:
@@ -62,6 +52,7 @@ def main():
                 hashtags.append(wall.hashtag)
                 stream = Stream(auth, l)
                 stream.filter(track=[wall.hashtag], async=True)
+                time.sleep(5)
 
 if __name__ == '__main__':
     main()
