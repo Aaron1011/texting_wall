445f441bfb7e3e16662a1d66ef56026674929409
Aaron Hill <aa1ronham@aol.com>
2012-10-31 16:54:01 -0400

    Updated PubnubListener class

--- listen_for_tweets.py
+++ listen_for_tweets.py
@@ -4,15 +4,12 @@ from tweepy import Stream
 from django.core.management import setup_environ
 from texting_wall import settings
 setup_environ(settings)
-
+from tweepy.api import API
 from main.models import Wall
 from main import Pubnub
 import json, re
 import time
-
-
-
-
+import sys, signal
 
 
 class PubnubListener(StreamListener):
@@ -20,12 +17,17 @@ class PubnubListener(StreamListener):
     This is a basic listener that just prints received tweets to stdout.
 
     """
+    def __init__(self, pubnub, api=None):
+        self.api = api or API()
+        self.pubnub = pubnub
+
     def on_data(self, data):
+        print data
         message = json.loads(data)
         tweet = re.match("(\S*)\s(.*)", str(message['text']))
         print tweet.group(1)
         wall = Wall.objects.filter(hashtag__exact=tweet.group(1))
-        pubnub.publish({
+        self.pubnub.publish({
             'channel' : wall[0].sms_keyword,
             'message' : {
                 'message' : tweet.group(2)
@@ -37,15 +39,12 @@ class PubnubListener(StreamListener):
         print status
 
 
-
 def main():
     pubnub = Pubnub.Pubnub(settings.PUBLISH_KEY, settings.SUBSCRIBE_KEY, settings.SECRET, False)
     hashtags = set()
 
     auth = OAuthHandler(settings.consumer_key, settings.consumer_secret)
     auth.set_access_token(settings.access_token, settings.access_token_secret)
-    print("Oauth Hander has consumer key {0} and consumer secret {1}".format(settings.consumer_key,settings.consumer_secret))
-    print("Oauth handler has access token {0} and access token secret {1}".format(settings.access_token, settings.access_token_secret))
     stream = None
     while True:
         current_hashtags = set(w.hashtag for w in Wall.objects.all())
