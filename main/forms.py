462a496f3f69ffa41eca8fcfa39bdd4fc61a6b99
Aaron Hill <aa1ronham@aol.com>
2012-11-08 20:43:42 -0500

    Added Twitter Bootstrap static files

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from models import TRAFFIC_SOURCE, Wall
 class UserCreation(UserCreationForm):
     about_textwall = forms.ChoiceField(TRAFFIC_SOURCE)
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={
+        'id': 'inputEmail', 'placeholder' : 'Email'
+        }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={ 'id':
+        'inputPassword', 'placeholder' : 'Password' 
+        }))
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user_cache = None
+        super(LoginForm, self).__init__(*args, **kwargs)
 
 class WallForm(forms.ModelForm):
     def clean_hashtag(self):
