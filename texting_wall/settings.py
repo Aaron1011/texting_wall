0c242825715b8f4b772cde160e86058cf6caf04f
Aaron Hill <aa1ronham@aol.com>
2012-11-09 15:03:41 -0500

    Added STATIC_ROOT path

--- texting_wall/settings.py
+++ texting_wall/settings.py
@@ -75,7 +75,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.abspath('staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -133,7 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    
+
 
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
