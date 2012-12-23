34f125c3dbaff7883de0a87102e629ef02768c50
Aaron Hill <aa1ronham@aol.com>
2012-12-23 15:14:35 -0500

    Added MessageSender to django admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
-from main.models import Wall, Message
+from main.models import Wall, Message, MessageSender
 from django.contrib import admin
 
 admin.site.register(Wall)
 admin.site.register(Message)
+admin.site.register(MessageSender)
