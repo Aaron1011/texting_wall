d5eeff4ecc9198d69024a2fb2ca41ee80ddd5a32
Aaron Hill <aa1ronham@gmail.com>
2013-06-08 17:20:52 -0400

    Changed acceptance tests to use TwitterListener

--- main/tests.py
+++ main/tests.py
@@ -14,6 +14,7 @@ from django.core.urlresolvers import reverse
 from django.core import management
 from os import environ
 from selenium import webdriver
+from main.management.commands._listen_for_tweets import TwitterListener 
 import random
 import tweepy
 import time
@@ -31,9 +32,12 @@ class SMSTest(TestCase):
         self.wall.save()
 
     def test_single_wall(self):
+        print "Single wall"
         self.assertEquals(_split_message('This is a sentence', ''), (self.wall.hashtag, 'This is a sentence'))
+        print "Done"
 
     def test_phone_number(self):
+        print "Phone number"
         self.wall2.save()
 
         self.assertEquals(_split_message('Test message', '+12223334444'), (None, None))
@@ -43,6 +47,7 @@ class SMSTest(TestCase):
         self.assertEquals(_split_message('Hello world',
             self.wall.phone_number), (self.wall.hashtag, 'Hello world'))
         self.assertEquals(_split_message('Hello world', '+12223334444'), (None, None))
+        print "Done"
 
     #def test_purchase_phone_number(self):
 
@@ -59,18 +64,22 @@ class WallTest(TestCase):
 class AcceptanceTest(LiveServerTestCase):
 
     def setUp(self):
+        print "Starting browser"
         self.browser = webdriver.Firefox()
         auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY,
                 settings.TWITTER_CONSUMER_SECRET)
         auth.set_access_token(settings.TWITTER_ACCESS_TOKEN,
                 settings.TWITTER_ACCESS_TOKEN_SECRET)
         self.twitter = tweepy.API(auth)
-        management.call_command('listen_for_tweets')
+
+        self.twitter_listener = TwitterListener()
 
     def tearDown(self):
         self.browser.close()
+        self.twitter_listener.exit()
 
     def test_send_tweet(self):
+        print "Browser test"
         self.browser.get(self.live_server_url + reverse('main.views.index'))
         self.assertEqual(self.browser.title, "Texting Wall")
         self.browser.get(self.live_server_url + reverse('main.views.create_account'))
@@ -83,14 +92,18 @@ class AcceptanceTest(LiveServerTestCase):
                 reverse('main.views.index'))
 
         self.browser.get(self.live_server_url + reverse('main.views.new_wall'))
-        hashtag = self.browser.find_element_by_id("inputHashtag")
-        hashtag.send_keys("#walltest")
-        hashtag.submit()
 
-        message = "#%s %s" % (random.random(), random.random())
+        twitter_hashtag = "#" + str(random.random())
+        message = "%s %s" % (twitter_hashtag, random.random())
+
+        hashtag = self.browser.find_element_by_id("inputHashtag")
+        hashtag.send_keys(twitter_hashtag)
+        hashtag.submit()
+        self.twitter_listener.update()
+
         print message
         self.twitter.update_status(message)
         time.sleep(.5)
         self.browser.find_element_by_xpath("//p[contains(text(), '%s')]" %
                 message)
-
+        print "Done"
